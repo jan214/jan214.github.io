e40a24c4025d1e99eeae4117347aa53b5be16a7a
jan214 <49987427+jan214@users.noreply.github.com>
2023-05-29 20:29:13 +0200

    Add files via upload

--- water/water.js
+++ water/water.js
@@ -196,7 +196,7 @@ var objAssign = Object.assign;
     }
 
     }
-    loadPackage({"files": [{"filename": "/waternormal.png", "start": 0, "end": 1137296}, {"filename": "/waterdisplacement.png", "start": 1137296, "end": 1554599}], "remote_package_size": 1554599, "package_uuid": "e62a4d39-2ca7-402a-9be3-85976c4adffc"});
+    loadPackage({"files": [{"filename": "/waternormal.png", "start": 0, "end": 1137296}, {"filename": "/waterdisplacement.png", "start": 1137296, "end": 1554599}], "remote_package_size": 1554599, "package_uuid": "72991ae1-185d-49ca-bcb2-bafc9ed46c1c"});
 
   })();
 
