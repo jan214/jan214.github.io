e201a0f4c463c4b1a0cd12113f428b7e03e51a48
jan214 <49987427+jan214@users.noreply.github.com>
2026-03-10 00:04:55 +0100

    New Collision Response
    Lever Arm and Torque seems mostly fixed on desktop, need testing on mobile.

--- MaterialTest/build/test.js
+++ MaterialTest/build/test.js
@@ -31,7 +31,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Jan\AppData\Local\Temp\tmpd_fa4jhs.js
+// include: C:\Users\Jan\AppData\Local\Temp\tmpic1u94jz.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -212,21 +212,21 @@ Module['FS_createPath']("/", "Resources", true, true);
 
   })();
 
-// end include: C:\Users\Jan\AppData\Local\Temp\tmpd_fa4jhs.js
-// include: C:\Users\Jan\AppData\Local\Temp\tmpj80dctg7.js
+// end include: C:\Users\Jan\AppData\Local\Temp\tmpic1u94jz.js
+// include: C:\Users\Jan\AppData\Local\Temp\tmpiguu7mpf.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Jan\AppData\Local\Temp\tmpj80dctg7.js
-// include: C:\Users\Jan\AppData\Local\Temp\tmp2yuby00q.js
+  // end include: C:\Users\Jan\AppData\Local\Temp\tmpiguu7mpf.js
+// include: C:\Users\Jan\AppData\Local\Temp\tmp1ehwuktm.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Jan\AppData\Local\Temp\tmp2yuby00q.js
+  // end include: C:\Users\Jan\AppData\Local\Temp\tmp1ehwuktm.js
 
 
 var arguments_ = [];
